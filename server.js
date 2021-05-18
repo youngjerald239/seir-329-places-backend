9bb89af3b7a1f7c4e7abe0d439ffdf91a973eef8
youngjerald239 <youngjerald239@gmail.com>
2021-05-17 21:24:29 -0500

    added missing middleware

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const placeRouter = require("./controllers/Place");
 NODE_ENV === "production" ? app.use(cors(corsOptions)) : app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny")); //logging
-
+app.use(cors())
 ///////////////
 //Routes and Routers
 //////////////
